74838435c2ebc763c5fc9ae6517a92e7d6cae785
Miguelito36 <jomiasga@gmail.com>
2024-11-02 01:28:28 -0500

    Delete} Customers

--- source/repos/SupermarketWEB/obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
+++ source/repos/SupermarketWEB/obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e97b39bd5bf93248c6e0a852a6239e8d730cac18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b3fae73e6568b08e670f0a5a7b84ebb7a631c6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
